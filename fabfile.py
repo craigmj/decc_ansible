fbf0ad0bb6dffda169219cae3a510e84192ea388
craigmj <craig@lateral.co.za>
2014-03-25 12:37:43 +0200

    deccgem tool

--- fabfile.py
+++ fabfile.py
@@ -9,14 +9,24 @@ webPath = basePath + "twenty_fifty"
 
 def deploy():
 	with cd(modelPath):
+		run("gem clean")
 		run("git reset --hard")
-		run("git pull")
-		run("gem build model.gemspec")
-		sudo("gem install decc_2050_model-3.5.1pre.gem")
+		run("git pull origin master")
+		sudo("gem install `deccgem whichgem`")
 	with cd(webPath):
 		run("git reset --hard")
-		run("git pull")
-		run("cp Gemfile.v351pre Gemfile")
-		run("gem clean")
-		run("gem install")
+		run("git pull origin master")
+		run("deccgem gemfile > Gemfile")
+		run("bundle install")
 		sudo("restart decc2050")
+
+# startup.sh
+# #!/bin/bash
+
+# gem clean
+# cd ../decc_2050_model
+# gem build model.gemspec
+# gem install decc_2050_model-0.71.20140319pre.gem
+# cd ../twenty-fifty
+# bundle install
+# rackup
